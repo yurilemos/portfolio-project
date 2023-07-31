bd9ef408245fbc7384ff82316eb7f7f585a278f6
Yuri Lemos <yuri.lemos@parafernalia.net.br>
2023-07-31 17:09:25 -0300

    add submenu on small size screens

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { HeaderContent, HeaderTitle, LinksContent } from "./styles";
+import { 
+  HeaderContent, 
+  HeaderTitle, 
+  LinksContent, 
+  SubMenuButton, 
+  SubMenu,
+  SubMenuItem,
+  ScrollButton
+} from "./styles";
+import { AiOutlineMenu } from "react-icons/ai";
+
 
 
 export const Header = () => {
+
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
   const scrollToSection = (id:string) => {
     const element = document.getElementById(id);
@@ -11,16 +23,38 @@ export const Header = () => {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
+
+  const changeSubmenu = ()=>{
+    setIsSubMenuOpen(current => !current)
+  }
   return (
     <HeaderContent>
       <HeaderTitle>My.portfolio</HeaderTitle>
-
       <LinksContent>
         {/* <Link to="/">Feedback</Link> */}
-        <button onClick={() => scrollToSection("experience")}>Experience</button>
-        <button onClick={() => scrollToSection("projects")}>Projects</button>
+        <ScrollButton onClick={() => scrollToSection("experience")}>Experience</ScrollButton>
+        <ScrollButton onClick={() => scrollToSection("projects")}>Projects</ScrollButton>
         {/* <Link to="/">About me</Link> */}
       </LinksContent>
+      <SubMenuButton onClick={()=>{changeSubmenu()}}>
+        <AiOutlineMenu style={{ width: '1.5rem', height: '1.5rem' }}/>
+      </SubMenuButton>
+      <SubMenu isopen={isSubMenuOpen.toString()}>
+        <ScrollButton 
+          onClick={() => {
+            scrollToSection("experience")
+            changeSubmenu()
+          }}>
+            Experience
+        </ScrollButton>
+        <ScrollButton 
+          onClick={() => {
+            scrollToSection("projects")
+            changeSubmenu()
+          }}>
+            Projects
+        </ScrollButton>        
+      </SubMenu>
     </HeaderContent>
   );
 };
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+
+interface SubMenuProps {
+  isopen?: string;
+}
+
+
+
+
 export const HeaderContent = styled.header`
   display: flex;
   justify-content: center;
@@ -14,25 +22,32 @@ export const HeaderTitle = styled.h2`
   left: 0;
 `;
 
+export const SubMenuButton = styled.button`
+  display: none;
+  background: none;
+  border: none;
+  color: var(--color-white);  
+  border-radius: 30px;
+  transition: background-color 0.5s ease;
+  padding: 0.2rem 0.3rem;
+
+  
+  &:active {
+    background-color: rgba(240, 248, 255, 0.1);
+  }
+
+  @media (max-width: 1030px) {
+    display: unset;
+    position: absolute;
+    right: 0;
+  }
+`;
+
 export const LinksContent = styled.div`
   display: flex;
   gap: 2rem;
   position: absolute;
 
-  button {
-    font-style: normal;
-    font-weight: 700;
-    font-size: 1rem;
-    color: var(--white-color);
-    text-decoration: none;
-    transition: font-size 0.3s ease;
-    cursor: pointer;
-    background: none;
-    border: none;
-  }
-  button:hover {
-    font-size: 1.1em;
-  }
 
   a {
     font-style: normal;
@@ -52,3 +67,41 @@ export const LinksContent = styled.div`
     display: none;
   }
 `;
+
+export const SubMenu = styled.div<SubMenuProps>`
+  display: ${({ isopen }) => (isopen === 'true' ? "flex" : "none")};
+  position: absolute;
+  top: 100%;
+  right: 0;
+  background-color: rgba(240, 240, 240, 0.5);
+  padding: 2rem 1rem;  
+  flex-direction: column;
+  gap: 2rem;
+  color: black;
+  z-index: 5;
+  align-items: baseline;
+`;
+
+export const SubMenuItem = styled.div`
+  padding: 0.5rem;  
+`;
+
+
+export const ScrollButton = styled.button`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1rem;
+  color: var(--white-color);
+  text-decoration: none;
+  transition: font-size 0.3s ease;
+  cursor: pointer;
+  background: none;
+  border: none;  
+
+  @media (min-width: 1031px) {
+    &:hover {
+      font-size: 1.1rem;
+    }
+  }
+`;
+
